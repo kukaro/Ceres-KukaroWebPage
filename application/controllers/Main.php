fcf2c97b7f100b78516f0f0c571547e4215668a5
KTJ <justkukaro@naver.com>
2018-01-07 00:09:04 +0900

    v0.1.14 import bootstrap,jquery

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -11,7 +11,15 @@ class Main extends CI_Controller
 
     public function index()
     {
-        $data = array("css"=>base_url("application/views/public/bootstrap/dist/css/bootstrap.min.css"));
-        $this->load->view('main',$data);
+        $css = array("bootstrap" => base_url("application/views/public/bootstrap/dist/css/bootstrap.min.css"),
+            "custom" => base_url("application/views/public/custom/dist/css/custom.css")
+        );
+        $js = array("jquery" => base_url("application/views/public/jquery/dist/jquery.min.js"),
+            "bootstrap" => base_url("application/views/public/bootstrap/dist/js/bootstrap.min.js")
+        );
+        $data = array("css" => $css,
+            "js" => $js);
+
+        $this->load->view('main', $data);
     }
 }
--- application/views/main.php
+++ application/views/main.php
@@ -11,47 +11,51 @@
     <title>Welcome Ceres!</title>
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="<?=$css?>" type="text/css">
+    <link rel="stylesheet" href="<?= $css['bootstrap'] ?>" type="text/css">
     <!-- Custom -->
-    <link rel="stylesheet" href="./application/views/public/custom/dist/css/custom.css" type="text/css">
+    <link rel="stylesheet" href="<?= $css['custom'] ?>" type="text/css">
 
 </head>
 <body>
-<nav class="navbar navbar-default" role="navigation">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">로고 </a>
-    </div>
+<div class="container">
+    <nav class="navbar navbar-default" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">로고 </a>
+        </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse navbar-ex1-collapse">
-        <ul class="nav navbar-nav">
-            <li class="active"><a href="#">메뉴1 </a></li>
-            <li><a href="#">메뉴2</a></li>
-            <li><a href="#">메뉴3</a></li>
-            <!-- <li class="dropdown">
-               <a href="#" class="dropdown-toggle" data-toggle="dropdown">드롭다운 <b class="caret"></b></a>
-               <ul class="dropdown-menu">
-                 <li><a href="#">서브메뉴 1</a></li>
-                 <li><a href="#">서브메뉴 2</a></li>
-                 <li><a href="#">서브메뉴 3</a></li>
-               </ul>
-             </li> -->
-        </ul>
-        <form class="navbar-form navbar-right" role="search">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="검색">
-            </div>
-            <button type="submit" class="btn btn-default">Submit</button>
-        </form>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">메뉴1 </a></li>
+                <li><a href="#">메뉴2</a></li>
+                <li><a href="#">메뉴3</a></li>
+                <!-- <li class="dropdown">
+                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">드롭다운 <b class="caret"></b></a>
+                   <ul class="dropdown-menu">
+                     <li><a href="#">서브메뉴 1</a></li>
+                     <li><a href="#">서브메뉴 2</a></li>
+                     <li><a href="#">서브메뉴 3</a></li>
+                   </ul>
+                 </li> -->
+            </ul>
+            <form class="navbar-form navbar-right" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="검색">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
 
-    </div><!-- /.navbar-collapse -->
-</nav>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</div>
+<script src="<?=$js['jquery']?>"></script>
+<script src="<?=$js['bootstrap']?>"></script>
 </body>
 </html>
