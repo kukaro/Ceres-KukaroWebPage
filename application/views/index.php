0daae077b0b82f071dbe6effa68ad3716ae8776a
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2018-01-06 10:24:24 +0900

    v0.1.11 add index.php change gitignore : include public/custom

---
+++ application/views/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="kr">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome Ceres!</title>
+
+    <!-- Bootstrap -->
+    <link href="public/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom -->
+    <link href="public/custom/dist/css/custom.css" rel="stylesheet">
+</head>
+<body>
+
+</body>
+</html>
