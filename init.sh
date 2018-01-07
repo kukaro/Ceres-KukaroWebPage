ebec79ee217893bbaef6cf1accb97cca2e6e8606
root <root@ip-172-31-13-177.ap-northeast-2.compute.internal>
2018-01-07 12:51:54 +0000

    v0.1.19 \n automatic insert bower shell script

--- init.sh
+++ init.sh
@@ -1,2 +1,21 @@
-mkdir -p file/user/admin
-mkdir -p code
+#!/bin/sh
+#띄어쓰기 하나하나 까지 다 지켜야함
+
+#make directory
+sudo mkdir -p file/user/admin
+sudo mkdir -p code
+
+#install npm
+npmIxist=`which npm | wc -c`
+bowerIxist=`which bower | wc -c`
+if [ $npmIxist -gt 0 ];then
+    echo "npm ixist"
+else
+    echo "npm not ixist"
+fi
+if [ $bowerIxist -gt 0 ];then
+    echo "bower ixist"
+else
+    echo "bower not ixist"
+    sudo npm install -g bower
+fi
