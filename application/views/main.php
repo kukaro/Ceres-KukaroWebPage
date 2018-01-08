55efaa93a41a2b7ee2d105f55dd77048f2f770f3
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2018-01-09 03:03:28 +0900

    v0.1.24 change navbar

--- application/views/main.php
+++ application/views/main.php
@@ -18,46 +18,48 @@
 </head>
 <body>
 <div class="container">
-    <nav class="navbar navbar-default" role="navigation">
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/CI/index.php/main"><img src="<?= $image['custom-ceres'] ?>"
-                                                  style="width: auto; max-height: 100%;"> </a>
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/CI/main"><img src="<?= $image['custom-ceres'] ?>"
+                                                             style="width: auto; max-height: 100%;"> </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li onmouseover="mouse_over(0)" onmouseleave="mouse_leave(0)"><a href="#">사이트 소개</a></li>
+                    <li onmouseover="mouse_over(1)" onmouseleave="mouse_leave(1)"><a href="#">파일 관리</a></li>
+                    <li onmouseover="mouse_over(2)" onmouseleave="mouse_leave(2)"><a href="#">코드 관리</a></li>
+                    <!-- <li class="dropdown">
+                       <a href="#" class="dropdown-toggle" data-toggle="dropdown">드롭다운 <b class="caret"></b></a>
+                       <ul class="dropdown-menu">
+                         <li><a href="#">서브메뉴 1</a></li>
+                         <li><a href="#">서브메뉴 2</a></li>
+                         <li><a href="#">서브메뉴 3</a></li>
+                       </ul>
+                     </li> -->
+                </ul>
+                <form class="navbar-form navbar-right" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="검색">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+
+            </div>
         </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul class="nav navbar-nav">
-                <li onmouseover="mouse_over(0)" onmouseleave="mouse_leave(0)"><a href="#">사이트 소개</a></li>
-                <li onmouseover="mouse_over(1)" onmouseleave="mouse_leave(1)"><a href="#">파일 관리</a></li>
-                <li onmouseover="mouse_over(2)" onmouseleave="mouse_leave(2)"><a href="#">코드 관리</a></li>
-                <!-- <li class="dropdown">
-                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">드롭다운 <b class="caret"></b></a>
-                   <ul class="dropdown-menu">
-                     <li><a href="#">서브메뉴 1</a></li>
-                     <li><a href="#">서브메뉴 2</a></li>
-                     <li><a href="#">서브메뉴 3</a></li>
-                   </ul>
-                 </li> -->
-            </ul>
-            <form class="navbar-form navbar-right" role="search">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="검색">
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
-
-        </div><!-- /.navbar-collapse -->
     </nav>
 </div>
 <script src="<?= $js['jquery'] ?>"></script>
 <script src="<?= $js['bootstrap'] ?>"></script>
-<script src="<?= $js['custom']?>"></script>
+<script src="<?= $js['custom'] ?>"></script>
 </body>
 </html>
--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # 생성 디렉터리 삭제
 rm -rf file
 rm -rf code
