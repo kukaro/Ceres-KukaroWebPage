3fdc3180e75cd4f8674f30dd3a6a089ac7d6cfae
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2018-01-09 02:01:23 +0900

    v0.1.23 repair address

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -11,14 +11,14 @@ class Main extends CI_Controller
 
     public function index()
     {
-        $css = array("bootstrap" => base_url("application/views/public/bootstrap/dist/css/bootstrap.min.css"),
-            "custom" => base_url("application/views/public/custom/dist/css/custom.css")
+        $css = array("bootstrap" => "application/views/public/bootstrap/dist/css/bootstrap.min.css",
+            "custom" =>"application/views/public/custom/dist/css/custom.css"
         );
-        $js = array("jquery" => base_url("application/views/public/jquery/dist/jquery.min.js"),
-            "bootstrap" => base_url("application/views/public/bootstrap/dist/js/bootstrap.min.js"),
-            "custom" => base_url("application/views/public/custom/dist/js/main.js")
+        $js = array("jquery" => "application/views/public/jquery/dist/jquery.min.js",
+            "bootstrap" => "application/views/public/bootstrap/dist/js/bootstrap.min.js",
+            "custom" => "application/views/public/custom/dist/js/main.js"
         );
-        $image = array("custom-ceres" => base_url("application/views/public/custom/dist/image/ceres_origin.bmp"));
+        $image = array("custom-ceres" => "application/views/public/custom/dist/image/ceres_origin.bmp");
         $data = array("css" => $css,
             "js" => $js,
             "image" => $image);
