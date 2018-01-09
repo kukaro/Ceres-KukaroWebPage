1208a678a13e1891a2e437c903a2773c91c758ba
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2018-01-10 07:06:42 +0900

    v0.1.42 add main page

--- application/views/main.php
+++ application/views/main.php
@@ -65,7 +65,20 @@
             </div>
         </div>
     </nav>
+    <article>
+        <div class="page-header">
+            <h1> Ceres - IT Web page</h1>
+        </div>
+        <div class="jumbotron">
+            <div class="container">
+                <h1>Hello, ceres!</h1>
+                <p>IT Web Page 입니다. </p>
+                <p><a class="btn btn-primary btn-lg">사이트 소개 보기</a></p>
+            </div>
+        </div>
+    </article>
 </div>
+
 <script src="<?= $js['jquery'] ?>"></script>
 <script src="<?= $js['bootstrap'] ?>"></script>
 <script src="<?= $js['custom'] ?>"></script>
