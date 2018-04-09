5b3c81c8a0abbe022316cca75380710ec235fd65
kukaro <justkukaro@naver.com>
2018-04-09 22:30:22 +0900

    Test

---
+++ CI/application/controllers/Ujin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiharu
+ * Date: 2018. 4. 9.
+ * Time: PM 10:29
+ */
+
+class Ujin
+{
+    public function index()
+    {
+        $this->load->view('ujin');
+    }
+}
---
+++ CI/application/views/ujin.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="kr">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    박유진 핵멍청
+</body>
+</html>
